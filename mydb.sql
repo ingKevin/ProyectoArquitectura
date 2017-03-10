1ec974efa00c40dd39c9c50d938dbc256b82f2e0
simondres <smunozriver@uniminuto.edu.co>
2017-03-10 02:20:53 -0500

    Base de Datos con ID Auto Incrementabley Relaciones

---
+++ mydb.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-03-2017 a las 08:16:56
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `idAdministrador` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`idAdministrador`, `usuario`, `password`) VALUES
+(1, 'adm', 'adm');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artista`
+--
+
+CREATE TABLE IF NOT EXISTS `artista` (
+  `idArtista` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `artista`
+--
+
+INSERT INTO `artista` (`idArtista`, `usuario`, `password`) VALUES
+(1, 'simon', '123'),
+(2, 'Mickey', 'Mouse'),
+(3, 'simon2', 'simon');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `camiseta`
+--
+
+CREATE TABLE IF NOT EXISTS `camiseta` (
+  `idEstampa` int(11) NOT NULL,
+  `talla` varchar(45) DEFAULT NULL,
+  `color` varchar(45) DEFAULT NULL,
+  `material` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `idCliente` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `ubicacion` varchar(45) DEFAULT NULL,
+  `formaPago` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`idCliente`, `usuario`, `password`, `ubicacion`, `formaPago`) VALUES
+(1, 'sim', '123', 'bog', 'x'),
+(2, 'Mickey', 'Mouse', 'Mouse', 'Mouse'),
+(3, 'Mickey2', 'Mouse', 'Mouse', 'Mouse');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estampa`
+--
+
+CREATE TABLE IF NOT EXISTS `estampa` (
+  `idEstampa` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `imagenes` varchar(45) DEFAULT NULL,
+  `tema` varchar(45) DEFAULT NULL,
+  `rating` varchar(45) DEFAULT NULL,
+  `popularidad` varchar(45) DEFAULT NULL,
+  `Artista_idArtista` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE IF NOT EXISTS `venta` (
+  `idVenta` int(11) NOT NULL,
+  `fecha` varchar(45) DEFAULT NULL,
+  `Cliente_idCliente` int(11) NOT NULL,
+  `Camiseta_idEstampa` int(11) NOT NULL,
+  `Artista_idArtista` int(11) NOT NULL,
+  `Estampa_idEstampa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`idAdministrador`);
+
+--
+-- Indices de la tabla `artista`
+--
+ALTER TABLE `artista`
+  ADD PRIMARY KEY (`idArtista`);
+
+--
+-- Indices de la tabla `camiseta`
+--
+ALTER TABLE `camiseta`
+  ADD PRIMARY KEY (`idEstampa`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `estampa`
+--
+ALTER TABLE `estampa`
+  ADD PRIMARY KEY (`idEstampa`),
+  ADD KEY `fk_Estampa_Artista1_idx` (`Artista_idArtista`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`idVenta`),
+  ADD KEY `fk_Venta_Cliente_idx` (`Cliente_idCliente`),
+  ADD KEY `fk_Venta_Camiseta1_idx` (`Camiseta_idEstampa`),
+  ADD KEY `fk_Venta_Artista1_idx` (`Artista_idArtista`),
+  ADD KEY `fk_Venta_Estampa1_idx` (`Estampa_idEstampa`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `idAdministrador` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `artista`
+--
+ALTER TABLE `artista`
+  MODIFY `idArtista` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `camiseta`
+--
+ALTER TABLE `camiseta`
+  MODIFY `idEstampa` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `idVenta` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
